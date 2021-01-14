b98ee3aca49df7e7be2ffb541146f041e5c8d4dc
fartyg <andreas.jademan@gmail.com>
2021-01-14 17:48:23 +0200

    upd

--- .scripts/hockey.py
+++ .scripts/hockey.py
@@ -11,15 +11,15 @@ import datetime as dt
 player = '/usr/bin/mpv'
 p = f'--custom-player {player}'
 
-# if you passed an int, thats your date
+now = dt.datetime.now()
+
 try:
-    d = int(sys.argv[1])
+    inp = int(sys.argv[1])
+    date = now + dt.timedelta(inp)
 
-# otherwise, yesterday is the date
 except:
-    now = dt.datetime.now()
-    prevday = now - dt.timedelta(1)
-    d = prevday.strftime('%Y/%m/%d').replace('/', '')
+    date = now
 
+d = date.strftime('%Y/%m/%d').replace('/', '')
 cmd = f'lazystream --date {d} play select {p}'
 os.system(cmd)
